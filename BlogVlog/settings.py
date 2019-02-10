a97c3574035484b50886b3e0b9f492295086b6a9
shailesh <ssshailesh3@gnail.com>
2019-02-11 01:02:35 +0530

    requirements.txt added

--- BlogVlog/settings.py
+++ BlogVlog/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
-
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -130,3 +130,6 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGIN_URL = 'login'
+
+
+django_heroku.settings(local())
